240e95d55bb5cd57220c47728a398d0a1579cba6
Sandipan Chatterjee <sandipan3002@gmail.com>
2024-02-09 14:43:28 +0530

    updated teampage

--- client/src/pages/Members.jsx
+++ client/src/pages/Members.jsx
@@ -1,37 +1,438 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography, IconButton } from '@mui/material'
+import { Box, } from '@mui/material'
 import { MemList } from '../Data/Data'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import "../../src/styles/members.css"
+
+import pankaj from "../image/1.jpg"
+import atri from "../image/2.jpg"
+import sandy from "../image/3.jpg"
+import ankit from "../image/4.jpg"
+import animesh from "../image/5.jpg"
+import abhigyan from "../image/6.jpg"
+import spandan from "../image/7.jpg"
+
+import ayush from "../image/21.jpg"
+import mehdi from "../image/22.jpg"
+import urnisha from "../image/23.jpg"
+import srinjay from "../image/24.jpg"
+import soumya from "../image/25.jpg"
+import faizan from "../image/26.jpg"
+import sampurno from "../image/27.jpg"
+import sayan from "../image/28.jpg"
+import anjali from "../image/29.jpg"
+import tania from "../image/210.jpg"
+
 
 const Members = () => {
   return (
     <Layout>
-        <Box sx={{ bgcolor: "black" ,display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-          {
-            MemList.map(mem => (
-              <Card sx={{maxWidth:'390px', display: "flex", m:3}}>
-                <CardActionArea>
-                  <CardMedia sx={{minHeight: "400px"}} component={'img'} src={mem.image} alt={mem.name}/>
-                  <CardContent>
-                  <Typography sx={{fontFamily:"cursive"}} variant='h5' gutterBottom component={'codename'}>
-                      "{mem.codename}"
-                    </Typography>
-                    <Typography sx={{fontFamily:"monospace", fontSize:"20px"}} variant='h8' gutterBottom component={'div'}>
-                      {mem.name}
-                    </Typography>
-                    <IconButton href={mem.linkedin} target="_blank"  color='primary'><LinkedInIcon/></IconButton>
-                    <IconButton color='inherit' href={mem.github}
-                    target="_blank" ><GitHubIcon/></IconButton>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            ))
-          }
-        </Box>
+      <Box sx={{}}>
+        <div>
+
+
+          <div class="responsive-container-block container">
+            <p class="text-blk team-head-text">
+              Core Team
+            </p>
+            <div class="responsive-container-block">
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={pankaj} />
+                  </div>
+                  <p class="text-blk name">
+                    Pankaj Barman
+                  </p>
+                  <p class="text-blk position">
+                    Lead Organizer
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={atri} />
+                  </div>
+                  <p class="text-blk name">
+                    Atri Sukul
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={sandy} />
+                  </div>
+                  <p class="text-blk name">
+                    Sandipan Chatterjee
+                  </p>
+                  <p class="text-blk position">
+                    Social media Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={ankit} />
+                  </div>
+                  <p class="text-blk name">
+                    Ankit Sengupta
+                  </p>
+                  <p class="text-blk position">
+                    Social media team
+                  </p>
+                  <p class="text-blk feature-text">
+                    ECE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={animesh} />
+                  </div>
+                  <p class="text-blk name">
+                    Animesh Bag
+                  </p>
+                  <p class="text-blk position">
+                    Backend Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={abhigyan} />
+                  </div>
+                  <p class="text-blk name">
+                    Abhigyan Prakash Singh
+                  </p>
+                  <p class="text-blk position">
+                    Graphics designing Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={spandan} />
+                  </div>
+                  <p class="text-blk name">
+                    Spandan Bhattacharya
+                  </p>
+                  <p class="text-blk position">
+                    Backend Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'25, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.twitter.com" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="responsive-container-block container">
+            <p class="text-blk team-head-text">
+              Extended Core Team
+            </p>
+            <div class="responsive-container-block">
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={ayush} />
+                  </div>
+                  <p class="text-blk name">
+                    Ayush Pandey
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/ayush-pandey-769238245/" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={mehdi} />
+                  </div>
+                  <p class="text-blk name">
+                    Mehdi Hasan
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/mehedi-hasan-10730b256/" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={urnisha} />
+                  </div>
+                  <p class="text-blk name">
+                    Urnisha Paul
+                  </p>
+                  <p class="text-blk position">
+                    Graphics designing Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="hhttps://www.linkedin.com/in/urnisha-paul-277689254" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={srinjay} />
+                  </div>
+                  <p class="text-blk name">
+                    Srinjay Mishra
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/srinjay-mishra-98609824a" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={soumya} />
+                  </div>
+                  <p class="text-blk name">
+                    Soumyabrata Sinha
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    IT'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/soumyabrata-sinha-a19aa5245" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={faizan} />
+                  </div>
+                  <p class="text-blk name">
+                    Faizan Ali Khan
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    IT'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/faizan-ali-khan-9a9b90255/" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={sampurno} />
+                  </div>
+                  <p class="text-blk name">
+                    Sampurno Sarkar
+                  </p>
+                  <p class="text-blk position">
+                    Graphics designing Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/sampurno-sarkar-376060219" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={sayan} />
+                  </div>
+                  <p class="text-blk name">
+                    Sayan Ganguly
+                  </p>
+                  <p class="text-blk position">
+                    Sponsor Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/sayan-ganguly-bw1729" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={anjali} />
+                  </div>
+                  <p class="text-blk name">
+                    Anjali Singh
+                  </p>
+                  <p class="text-blk position">
+                    Sponsor Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/anjali-singh-49aa55255" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 card-container">
+                <div class="card">
+                  <div class="team-image-wrapper">
+                    <img class="team-member-image" src={tania} />
+                  </div>
+                  <p class="text-blk name">
+                    Tania banerjee
+                  </p>
+                  <p class="text-blk position">
+                    Web Team
+                  </p>
+                  <p class="text-blk feature-text">
+                    CSE'26, JGEC
+                  </p>
+                  <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/taniaban2712" target="_blank">
+                      <LinkedInIcon fontSize="large"></LinkedInIcon>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </Box>
     </Layout>
-    
+
   )
 }
 
