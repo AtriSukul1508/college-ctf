3137d951f0acdf50073547114f30eb703f9e8e4f
Sandipan Chatterjee <sandipan3002@gmail.com>
2024-02-10 19:44:06 +0530

    updated about and challenges

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,70 +2,90 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 import { Box, Typography } from '@mui/material'
 import ParticlesBackground from '../components/Layout/ParticlesBackground'
-
-
+import '../styles/about.css'
 
 
 const About = () => {
+          // var now = new Date().getTime();
+          
+          // var hours = Math.floor((now%(1000*60*60*24))/(1000*60*60))
+          //   var minutes = Math.floor((now%(1000*60*60))/(1000*60))
+          //   var seconds = Math.floor((now%(1000*60))/1000)
+
+          //   document.getElementById("hours").innerHTML = hours
+          //   document.getElementById("minutes").innerHTML = minutes
+          //   document.getElementById("seconds").innerHTML = seconds       
   return (
     // <Box sx={{bgcolor:"black"}}>
     <Layout>
-        <ParticlesBackground/>
-        <Box sx={{
-          my: 15,
-          textAlign: "center",
-          p:2,
-          "& h4":{
-            fontWeight: 'bold',
-            my:2,
-            fontSize: "3rem"
-          },
-          "& p":{
-            margin: "10px",
-            color: "green",
-            textAlign: 'justify',
-            fontSize:"1.3rem"
-          },
-          "& ul":{
-            color:"green",
-            fontSize:"1.3rem",
-            margin:"20px",
-          },
-          "& h1":{
-            // display:"flex",
-            // justifyContent:'left',
-            color:"#ff004e",
-          },
-          "@media (max-width:600px)": {
-            mt: 0,
-            "& h4 ": {
-              fontSize: "1.5rem",
-            }
-          }
-        }}>
-          <Typography variant="h4" color="#ff004e">
-            Welcome to the Slay_Ctf
-          </Typography>
-          <Typography variant="h8" color="#ff004e">
-            by Z3R0_LOGON <span style={{color:'white'}}>x</span> CSE dept. JGEC
-          </Typography>
-          <p>
-          SLAY CTF is a hybrid Jeopardy-style contest where participants can test their cybersecurity skills and compete for prizes.
-          Z3R0_L0G0N is the cyber security club of JGEC. We are trying to build a community in North Bengal to learn from others and share our knowledge. We hope the participants will be encourage to learn more on this field through the CTF.</p>
-          <br/>
-          <h1>RULES :</h1>
-          <ul>
-              <li style={{textAlign:"left"}}>Participants are allowed to use any tool on the challenges (not on the website)</li>
-              <li style={{textAlign:"left"}}>Flag sharing is prohibited (if caught then we will directly disqualify them)</li>
-              <li style={{textAlign:"left"}}> Time duration : 2.30 hour  </li>
-        </ul>
-        <br></br>
-        <p>For further information follow us on the Social Media Handles Given Below</p>
+      <Box sx={{
+        color: "white",
 
-        </Box>
+      }}>
+        <ParticlesBackground/>
+
+        <div className="container">
+          <h1>Welcome to the SLAY_<span id='slay'>CTF</span></h1>
+          <span id='present'>presented by Z3R0_LOGON x Dept. of CSE, JGEC</span>
+
+         <div className="rule">
+         <p className='ruleh'>Rules</p>
+          <ul>
+            <li id="r">Participants are allowed to use any tool (online / offline) for solving the challenges</li>
+            <li id="r">Flag sharing is strictly prohibited. It will cause ban from the CTF for both the players</li>
+            <li id="r">The time duration of the CTF is 2.30 hours</li>
+            <li id="r">Any type of attack on the CTF website is strictly prohibited</li>
+          </ul>
+         </div>
+
+
+          <div className="faq">
+            <p className='faqh'>FAQ</p>
+            <ul>
+              <li id="q">What is the SLAY_CTF?</li>
+              <p id="a">
+                {'>'} SLAY_CTF is an hybrid mode jeopardy-CTF competition. 
+                "Capture The Flag" (CTF) competitions are not related
+                to running outdoors or playing first-person shooters. Instead,
+                they consist of a set of computer security puzzles (or challenges)
+                involving reverse-engineering, memory corruption, cryptography,
+                web technologies, and more. When players solve them they get a "flag",
+                a secret string which can be exchanged for points. The more points
+                a player earns, the higher up they move in rank.
+              </p>
+
+              <li id="q">What is the prize?</li>
+              <p id="a">
+              {'>'} The top 2 from both online and onsite winners will get exciting prizes. The participants
+                will get participation certificate and goodies.
+              </p>
+
+              <li id="q">How does it work?</li>
+              <p id="a">
+              {'>'} If this is your first time playing a CTF, take a look at 
+                this <a href="https://www.youtube.com/watch?v=8ev9ZX9J45A"> video</a>, and 
+                this <a href="https://trailofbits.github.io/ctf/">guide</a> .
+                In summary, we will release several challenges during the CTF, and each challenge has a
+                secret value (a "flag") with the format slay{'{flag}'}. If you find the flag, you can submit it for points.
+              </p>
+
+              <li id="q">Where can I ask a question?</li>
+              <p id="a">
+                {'>'} You can reach us by email at <a href="mailto:contact.slayctf@gmail.com">contact.slayctf@gmail.com</a> or join the <a href="https://discord.gg/2ATZUc596Q">Discord</a> .
+              </p>
+
+            </ul>
+          </div>
+
+
+
+        </div>
+
         
+
+      </Box>
+
     </Layout>
-    // </Box>
   )
 }
 
--- client/src/pages/Challenges.jsx
+++ client/src/pages/Challenges.jsx
@@ -1,15 +1,69 @@
 import { Typography } from "@mui/material";
 import Layout from "../components/Layout/Layout";
 import ParticlesBackground from "../components/Layout/ParticlesBackground";
+import '../styles/challenges.css'
 
 
-export default function Challenges(){
-    return(
+export default function Challenges() {
+
+    var countDownDate = new Date("Feb 17, 2024 14:00:00").getTime();
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+        document.getElementById("days").innerHTML = days
+        document.getElementById("hours").innerHTML = hours
+        document.getElementById("minutes").innerHTML = minutes
+        document.getElementById("seconds").innerHTML = seconds
+
+
+        if (distance < 0) {
+            clearInterval(x)
+            document.getElementById("days").innerHTML = "00"
+            document.getElementById("hours").innerHTML = "00"
+            document.getElementById("minutes").innerHTML = "00"
+            document.getElementById("seconds").innerHTML = "00"
+        }
+
+    }, 1000)
+
+
+    return (
         <Layout>
-            <ParticlesBackground/>
-            <Typography variant='h2' sx={{display: "flex", justifyContent: "center", height: "650px", alignItems: "center", color:"green"}}>
-                Challenges coming sooon!!
+            <ParticlesBackground />
+            <Typography variant='h2' sx={{ display: "flex", justifyContent: "center", height: "750px", alignItems: "center", color: "green" }}>
+                <div>
+                    <h1><span id="aaa">Challenges</span> coming sooon!!</h1><br />
+
+                    <div class="launch-time">
+                        <div>
+                            <p id="days">00</p>
+                            <span>Days</span>
+                        </div>
+                        <div>
+                            <p id="hours">00</p>
+                            <span>Hours</span>
+                        </div>
+                        <div>
+
+                            <p id="minutes">00</p>
+                            <span>Minutes</span>
+                        </div>
+                        <div>
+
+                            <p id="seconds">00</p>
+                            <span>Seconds</span>
+                        </div>
+                    </div>
+                </div>
             </Typography>
         </Layout>
     )
+
+
 }
