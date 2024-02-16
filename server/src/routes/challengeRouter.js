6c7167015df25cb96bcb93bf25127ac26180b5f2
Animesh <2025.cse18.animeshbag@gmail.com>
2024-02-16 20:29:45 +0530

    getChallenges function added

--- server/src/routes/challengeRouter.js
+++ server/src/routes/challengeRouter.js
@@ -1,9 +1,10 @@
 const challengeRouter = require('express').Router();
 
-const { createChallenge, updateScore } = require("../controllers/challengeConroller");
+const { createChallenge, updateScore, getChallenges } = require("../controllers/challengeConroller");
 const verifyAuth = require('../middlewares/authorization');
 
 challengeRouter.route("/challenge").post(createChallenge);
 challengeRouter.route("/solution").post(verifyAuth, updateScore);
+challengeRouter.route("/challenge").get(getChallenges);
 
 module.exports = challengeRouter;
